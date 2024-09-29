a0e7473d4a3982270fbbdd4de9ef05c79e66514e
sulai <sulai@DESKTOP-N90LMAC>
2024-09-29 23:50:27 +0100

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Write your word");
+            string word = Console.ReadLine();
+            word = word.ToLower();
+            word = Convert.ToString(word);
+            int wordlength = word.Length;
+            char wordone = word[0];
+            word = word.Remove(0, 1);
+            word = word + wordone + "ay";
+            Console.WriteLine(word);
+            Console.ReadKey();
+        }
+    }
+}
